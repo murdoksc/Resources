9242d4220e3cd040e9ef42848acea77d0d87ae85
carlos ivan gutierrez correa <gosu2424@hotmail.com>
2021-02-18 10:06:16 -0600

    first commit

---
+++ Section 6- Routing/start/router-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="Newest" {...props} />}
+            ></Route>
+            <Route path="/posts/:year?/:month?" component={Posts}></Route>
+            <Route path="/admin" component={Dashboard}></Route>
+            <Redirect from="/messages" to="/posts"></Redirect>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/" exact component={Home}></Route>
+            {<Redirect to="/not-found"></Redirect>}
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
